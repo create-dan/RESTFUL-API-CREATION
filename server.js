ff31a7d9e92ebe21d9a13d5862c87149f7226493
Dnyaneshwar <dnyaneshwars0611@gmail.com>
2021-11-13 09:05:43 +0530

    uploading once again same project

---
+++ server.js
@@ -0,0 +1,31 @@
+require('dotenv').config();
+
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+
+const app = express();
+mongoose.connect(process.env.DATABASE_URL ,{
+    useNewUrlParser: true,
+     useUnifiedTopology: true
+})
+
+
+const db = mongoose.connection;
+db.on('error' , (error) => console.log("error"));
+db.once('open' , () => console.log("connected to db"));
+
+
+
+app.use(express.json());
+
+const subscribersRouter = require('./routes/subscribers')
+app.use('/subscribers' , subscribersRouter);
+
+
+
+
+app.listen(3000 , () => {
+    console.log("server started");
+})
